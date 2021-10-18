c37287896dfca8d01b7469a169692b7043b81131
yoga-au <yogaagung.utama@gmail.com>
2021-10-18 15:13:25 +0700

    Add: Poppins font, color theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/poppins";
+import { ThemeProvider } from "styled-components";
+import type { AppProps } from "next/app";
+
+const theme = {
+  blue: "hsla(200, 94%, 53%, 1)",
+  gray: "hsla(0, 0%, 96%, 1)",
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
